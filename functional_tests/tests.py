4d07ee060a90434b27cf07e7d3f5f6d7ed91efa8
Stallmanifold <stallmanifold@gmail.com>
2017-10-08 04:31:36 +0000

    Update functional tests.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -12,13 +15,20 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
-
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Let us tell the tale of Bob Bobberson, the infamous terrorist from Far 
         # West Syria (a neighborhood in suburban Dearborn, Michigan.)
         # Bob Bobberson wants to buy supplies for making bombs for his terrorist
@@ -47,7 +57,7 @@ class NewVisitorTest(LiveServerTestCase):
         # "1: Buy chemicals." as an item in the to-do list.
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        self.check_for_row_in_list_table('1: Buy chemicals.')
+        self.wait_for_row_in_list_table('1: Buy chemicals.')
 
         # There is a text box inviting him to add more items. Having a lot of
         # things he needs to do, he writes out the instructions for making bombs and
@@ -58,8 +68,8 @@ class NewVisitorTest(LiveServerTestCase):
         time.sleep(1)
 
         # The page updates again, and now shows all items on his list.
-        self.check_for_row_in_list_table('1: Buy chemicals.')
-        self.check_for_row_in_list_table('2: Build chemical lab equipment.')
+        self.wait_for_row_in_list_table('1: Buy chemicals.')
+        self.wait_for_row_in_list_table('2: Build chemical lab equipment.')
 
         # Bob Bobberson wonders whether To-Do list, being owned by a greedy corporation
         # and full of analytics malware, will end up in the hands of a major world power
@@ -68,13 +78,12 @@ class NewVisitorTest(LiveServerTestCase):
         # shrugs and figures nothing much will happen. Bob continues entering items into his
         # list including the last two items:
         # ...
-        # 58. Buy beer and cigarettes.
-        # 59. Buy porn.
+        # 58. Buy beer.
+        # 59. Buy cigarettes.
         # ...
         # Bob is curious whether the website will remember his list when he leaves. So he 
         # closes the browser tab, and goes to the website again, and upon returning, he sees
         # that his list is exactly how he left it.
-        self.fail('Finish the test!!')
 
         # Satisfied, Bob Bobberson shuts down his computer and goes to sleep.
 
@@ -82,3 +91,49 @@ class NewVisitorTest(LiveServerTestCase):
         # One hour later, while Bob is dead asleep, a missile comes flying through his
         # window and he and his apartment are blown to smithereens. We never hear 
         # from Bob Bobberson the infamous chuckleheaded terrorist ever again.
+
+    
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith starts a new to-do list.
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # She notices that her list has a unique URL.
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, 'lists/.+')
+
+        # Now a new user, Francis, comes along to the site.
+
+        ## We use a new browser session to make sure no information
+        ## of Edith's is coming through from cookies, etc.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visits the home page. There is no sign of Edith's
+        # list.
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis starts a new list by entering a new item. He
+        # is less interesting than Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        # Francis gets his own unique URL.
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trace of Edith's list.
+        page_text = self.browser.find_elements_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # Satisfied, they both go back to sleep.

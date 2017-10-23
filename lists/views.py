998712ac3f62e21ee5f019c1aaedf5b535b84b59
Stallmanifold <stallmanifold@gmail.com>
2017-10-23 11:35:32 +0000

    Finish adding tests for forms.

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 
-from lists.forms import ItemForm
+from lists.forms import ExistingListItemForm, ItemForm
 from lists.models import List
 
 
@@ -12,7 +12,7 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        form.save(for_list=list_)
+        form.save(list_)
         return redirect(list_)
     else:
         return render(request, 'lists/home.html', {'form': form})
@@ -20,11 +20,11 @@ def new_list(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    form = ItemForm()
+    form = ExistingListItemForm(for_list=list_)
     if request.method == 'POST':
-        form = ItemForm(data=request.POST)
+        form = ExistingListItemForm(for_list=list_, data=request.POST)
         if form.is_valid():
-            form.save(for_list=list_)
+            form.save()
             return redirect(list_)
 
     return render(request, 'lists/list.html', {'list': list_, 'form': form})

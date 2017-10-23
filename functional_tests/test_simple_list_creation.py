3a9a5cc8da574fd30d86cab495cdd85ed415a853
Stallmanifold <stallmanifold@gmail.com>
2017-10-23 02:01:48 +0000

    Rename all item input ids and names. Still broken.

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -25,7 +25,7 @@ class NewVisitorTest(FunctionalTest):
 
         # Immediately on the homepage he is invited to make a list.
         # He types out his list of supplies he needs to buy.
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -41,7 +41,7 @@ class NewVisitorTest(FunctionalTest):
         # There is a text box inviting him to add more items. Having a lot of
         # things he needs to do, he writes out the instructions for making bombs and
         # weapons.
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Build chemical lab equipment.')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
@@ -74,7 +74,7 @@ class NewVisitorTest(FunctionalTest):
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new to-do list.
         self.browser.get(self.live_server_url)
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
@@ -99,7 +99,7 @@ class NewVisitorTest(FunctionalTest):
 
         # Francis starts a new list by entering a new item. He
         # is less interesting than Edith...
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
